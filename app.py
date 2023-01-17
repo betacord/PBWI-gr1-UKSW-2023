e047e7d4b70a4b9e6ca7a659cc3fe26f1a5a8fe2
tomek <t-krzywicki@wp.pl>
2023-01-17 18:46:02 +0100

    dodana interakcja z UI w czasie rzeczywistym

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')  # http://localhost:5000/
+@app.route('/')
 def index():
-    return 'to jest strona glowna!'
-
-
-@app.route('/contact')
-def contact():
-    return 'tel.: 997, 24/7'
+    return render_template(
+        'index.html',
+        img_src='https://i.ytimg.com/vi/7p6JPZf8c8g/hqdefault.jpg',
+        clip_title='Don Braden with Freddie Hubbard in Warsaw 1991 - Bolivia',
+        clip_description='This performance of Cedar Walton''s "Bolivia" is from Don Braden''s last gig with Freddie '
+                         'Hubbard, after a 2-1/2 year stint.  It was performed in Warsaw, Poland in October of 1991, '
+                         'and features Jeff Chambers on bass, Ralph Penland on drums, Ronnie Matthews on piano, '
+                         'Don on tenor sax, and the Master, Freddie Hubbard on trumpet.',
+    )
 
 
 if __name__ == '__main__':
