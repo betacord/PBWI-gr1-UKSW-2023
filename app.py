68d32031eb947136fcec9025be9e151d6f5514af
tomek <t-krzywicki@wp.pl>
2023-01-17 20:25:22 +0100

    dodana integracja z DB

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
+
+from db import db
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
